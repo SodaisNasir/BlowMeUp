053e42b4f34756b8cb6dc8e457f6cbb69bc2383e
farzam <89484118+FarzamN@users.noreply.github.com>
2023-04-07 12:42:16 +0500

    initial commit

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {} from 'react-native';
+import UserNavigator from './src/navigation/UserNavigator';
+
+const App = () => {
+  return <UserNavigator />;
+};
+
+export default App;
---
+++ src/navigation/UserNavigator.jsx
@@ -0,0 +1,86 @@
+import React from 'react';
+import {StyleSheet, Image, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {scale, verticalScale} from 'react-native-size-matters';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+import Dashboard from '../screens/users/Dashboard';
+
+const UserNavigator = () => {
+  const Tab = createBottomTabNavigator();
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName="home"
+        activeColor="#556084"
+        inactiveColor="#8EBCFE"
+        screenOptions={{
+          tabBarHideOnKeyboard: false,
+          tabBarShowLabel: true,
+          headerShown: false,
+          // tabBarActiveTintColor: '#8EBCFE',
+        }}>
+        <Tab.Screen
+          name="dashboard"
+          component={AllDashboard}
+          options={{
+            tabBarIcon: ({color}) => (
+              <Ionicons name="home" color={color} size={scale(20)} />
+            ),
+          }}
+        />
+        {/* <Tab.Screen
+          name="dashboard"
+          component={AllPic}
+          options={{
+            tabBarIcon: ({focused}) => (focused ? <Image /> : <Image />),
+          }}
+        />
+        <Tab.Screen
+          name="dashboard"
+          component={AllMid}
+          options={{
+            tabBarIcon: ({focused}) => (focused ? <Image /> : <Image />),
+          }}
+        />
+        <Tab.Screen
+          name="dashboard"
+          component={AllVlog}
+          options={{
+            tabBarIcon: ({focused}) => (focused ? <Image /> : <Image />),
+          }}
+        />
+        <Tab.Screen
+          name="dashboard"
+          component={AllSetting}
+          options={{
+            tabBarIcon: ({focused}) => (focused ? <Image /> : <Image />),
+          }}
+        /> */}
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default UserNavigator;
+
+const styles = StyleSheet.create({});
+
+const Stack = createNativeStackNavigator();
+
+function AllDashboard() {
+  return (
+    <Stack.Navigator
+      screenOptions={{headerShown: false}}
+      initialRouteName="userdashboard">
+      <Stack.Screen
+        name="userdashboard"
+        component={Dashboard}
+        options={{animation: 'slide_from_left'}}
+      />
+    </Stack.Navigator>
+  );
+}
